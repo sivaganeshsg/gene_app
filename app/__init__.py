811e868813641f9cf6852af0e13b26883d554fa9
Siva <sganesh@championsukplc.com>
2019-11-13 22:33:03 +0000

    catch errors and exception - so that it is can prevent leaking sensitive info. about the server

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,34 @@ def create_app():
     with app.app_context():
         from . import api
         app.register_blueprint(api.bp)
+        register_error_handlers(app)
         return app
+
+
+def register_error_handlers(app):
+    @app.errorhandler(404)
+    def not_found(error):
+        error_msg = str(error)
+        response = {"messages": error_msg}
+        return jsonify(response), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        error_msg = str(error)
+        response = {"messages": error_msg}
+        return jsonify(response), 405
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        print(str(error))
+        # Hide the "error" from the visitor. It can be stored to a file/db/service to log and identify the issue
+        error_msg = "Something went wrong"
+        response = {"messages": error_msg}
+        return jsonify(response), 500
+
+    @app.errorhandler(Exception)
+    def unhandled_exception(ex):
+        # Hide the "ex" from the visitor. It can be stored to a file/db/service to log and identify the issue
+        error_msg = "Something went wrong"
+        response = {"messages": error_msg}
+        return jsonify(response), 500
