6e5b80e42dfe508255d2cf819d5fcff6826e79e8
Siva <sganesh@championsukplc.com>
2019-11-14 01:01:58 +0000

    remove printing of the 500 error

--- app/__init__.py
+++ app/__init__.py
@@ -33,7 +33,6 @@ def register_error_handlers(app):
 
     @app.errorhandler(500)
     def internal_server_error(error):
-        print(str(error))
         # Hide the "error" from the visitor. It can be stored to a file/db/service to log and identify the issue
         error_msg = "Something went wrong"
         response = {"messages": error_msg}
